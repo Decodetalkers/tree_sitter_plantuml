ccf6ebfa049a451b4f24d4599e6a4f93454ca83e
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-04-09 13:07:19 +0800

    feat: support style

--- grammar.js
+++ grammar.js
@@ -11,9 +11,60 @@ module.exports = grammar({
   ],
 
   rules: {
-    plantuml: ($) => choice($.baseuml, $.yamluml, $.jsonuml),
+    plantuml: ($) =>
+      choice($.baseuml, $.gannttuml, $.saltuml, $.yamluml, $.jsonuml),
     ...yamluml,
     ...baseuml,
     ...jsonuml,
+    command: ($) => seq(repeat1($._command_unit), "\n"),
+    _command_unit: ($) =>
+      choice(
+        $.message,
+        $.block,
+        $.string,
+        $.method,
+        $.uniqkey,
+        $.color,
+        $.identifier,
+        $.comment
+      ),
+    method: ($) => seq($.identifier, "()"),
+    message: ($) => seq("$", $.identifier, "(", optional($.string), ")"),
+    block: ($) => choice($._block_1, $._block_2, $._block_3, $.block_style),
+    _block_1: ($) => seq("{", repeat($.command), "}"),
+    _block_2: ($) => seq("[", repeat($._command_unit), "]"),
+    _block_3: ($) => seq("(", repeat($._command_unit), ")"),
+    block_style: ($) => seq("<style>", repeat($.command), "</style>"),
+    uniqkey: ($) =>
+      choice(".", ">", "<", "_", ":", "*", "+", "-", "#", "!", "/", ",", "|"),
+    string: ($) =>
+      seq(
+        '"',
+        optional(
+          repeat(choice(token.immediate(prec(1, /[^"\\]+/)), $.escape_sequence))
+        ),
+        '"'
+      ),
+    escape_sequence: ($) =>
+      token.immediate(
+        seq(
+          "\\",
+          choice(
+            /[^xu0-7]/,
+            /[0-7]{1,3}/,
+            /x[0-9a-fA-F]{2}/,
+            /u[0-9a-fA-F]{4}/,
+            /u{[0-9a-fA-F]+}/
+          )
+        )
+      ),
+    color: ($) => seq($.colorleader, $.identifier),
+    colorleader: ($) => "#",
+    identifier: ($) => /[a-zA-Z0-9]+/,
+    // TODO: mutiline comment
+    comment: ($) => choice($._signallinecomment, $._mutilinecomment),
+    _signallinecomment: (_) => token(seq("'", /[^\n]+/g)),
+
+    _mutilinecomment: (_) => seq("/'", repeat(/[^\n^']+/g), "'/"),
   },
 });
